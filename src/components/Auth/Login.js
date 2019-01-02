98e6bb6be50e4fd402a1ebd2f296b2d851cdc5bc
mariapesm <youandmecanyousee@hotmail.com>
2019-01-02 23:02:03 +0200

    Update Login.js

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -35,7 +35,7 @@ class Login extends Component {
         <h2>You must be logged in to confirm any locations or see your friends.</h2>
         <div>
           <FacebookLogin
-            appId="283468548968368"
+            appId="2148026548597744"
             autoLoad={false}
             fields="name, friends, picture"
             scope="public_profile,user_friends"
