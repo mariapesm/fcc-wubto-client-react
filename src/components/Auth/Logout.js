d22195ac0566905b9cbc5fbaa6ef287eadea8ed2
mariapesm <youandmecanyousee@hotmail.com>
2019-01-02 23:02:29 +0200

    Update Logout.js

--- src/components/Auth/Logout.js
+++ src/components/Auth/Logout.js
@@ -33,7 +33,7 @@ class Logout extends Component {
         <h2>You are now logged out.</h2>
         <div>
           <FacebookLogin
-            appId="283468548968368"
+            appId="2148026548597744"
             autoLoad={false}
             fields="name, friends, picture"
             scope="public_profile,user_friends"
