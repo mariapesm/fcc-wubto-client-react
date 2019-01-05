7a22d0aff86e1d7a204d31d600c2ba93e52aa609
mariapesm <youandmecanyousee@hotmail.com>
2019-01-05 17:25:13 +0200

    Update Expired.js

--- src/components/Auth/Expired.js
+++ src/components/Auth/Expired.js
@@ -27,7 +27,7 @@ class Expired extends Component {
         <h2>Your session expired.</h2>
         <div>
           <FacebookLogin
-            appId="1099856153519121"
+            appId="2148026548597744"
             autoLoad={false}
             fields="name, friends, picture"
             scope="public_profile,user_friends"
