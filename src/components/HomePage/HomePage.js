ac21beb332cd3f205311f418f22ff7b079c6905a
mariapesm <youandmecanyousee@hotmail.com>
2019-01-05 16:34:47 +0200

    Update HomePage.js

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -48,7 +48,7 @@ class HomePage extends Component {
         {!this.props.authenticated && (
           <section>
             <FacebookLogin
-              appId="283468548968368"
+              appId="1099856153519121"
               autoLoad={false}
               fields="name, friends, picture"
               scope="public_profile,user_friends"
